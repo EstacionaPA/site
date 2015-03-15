fd75775c9d44c7bb11ee7abdd731f5bf7826ec57
Alfredo Eduardo <alfredudu_007@hotmail.com>
2015-03-15 17:01:55 -0300

    Retirado comentários restantes do arquivo webhook.php

--- webhook.php
+++ webhook.php
@@ -7,17 +7,14 @@ $REMOTE_REPO        = "git@github.com:EstacionaPA/site.git";
 $BRANCH             = "master";
 
 if ( $_POST['payload'] ) {
-  // Only respond to POST requests from Github
-  
+
   if( file_exists($LOCAL_REPO) ) {
-    
-    // If there is already a repo, just run a git pull to grab the latest changes
+   
     shell_exec("cd {$LOCAL_REPO} && git pull");
 
     die("done " . mktime());
   } else {
     
-    // If the repo does not exist, then clone it into the parent directory
     shell_exec("cd {$LOCAL_ROOT} && git clone {$REMOTE_REPO}");
     
     die("done " . mktime());
