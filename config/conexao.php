4dcbbd4624160becef8f09ab143d9831f85f788b
Alfredo Eduardo <alfredudu_007@hotmail.com>
2015-03-22 13:20:08 -0300

    Corrigido conexão com o banco de dados

--- config/conexao.php
+++ config/conexao.php
@@ -2,10 +2,7 @@
 
 // Conexao com o banco de dados
 
-$server = "localhost"; 
-$user = "root"; 
-$senha = "snake007"; 
-$base = "projeto"; 
-$conexao = mysql_connect($server, $user, $senha) or die("Erro na conexão!");
+$conexao = mysql_pconnect("localhost","root","snake007") or die ("Erro na conexão!");
+$banco = mysql_select_db("projeto");
 
 ?>
