aabd6fbeb5dc3003e92e89b017201766ee433375
Andre Luiz <brasilandre93@hotmail.com>
2015-03-22 00:09:23 -0300

    Mudanças no slide e no posicionamento do mesmo e Inserção de informação na pagina "Sobre a empresa".

--- css/script/script.js
+++ css/script/script.js
@@ -1,16 +1,7 @@
 // JavaScript Document
 
-$(function() {
-		
-			$("#slideshow > div:gt(0)").hide();
-	
-			setInterval(function() { 
-			  $('#slideshow > div:first')
-			    .fadeOut(300)
-			    .next()
-			    .fadeIn(500)
-			    .end()
-			    .appendTo('#slideshow');
-			},  2000);
-			
-		});
+$(document).ready(function(){
+  $('#slide').cycle( {
+    fx: 'fade'
+  } );
+});
