8ee4bc9fad500467c393b78dc395c535945334c9
Andre Luiz <brasilandre93@hotmail.com>
2015-03-29 18:11:39 -0300

    Corrição do login_user

--- php/login_user.php
+++ php/login_user.php
@@ -1,29 +1,3 @@
-<?php 
-
-include_once('../config/conexao_remoto.php');
-
-$usuario = $_POST['usuario'];
-$senha = $_POST['senha'];
-       
-if (!($usuario) || !($senha)){
-
-    print "Preencha todos os campos!"; exit();
-}
-
-//Abrindo Conexao com o banco de dados
-//$conexao = mysql_pconnect("localhost","root","snake007") or die (mysql_error());
-//$banco = mysql_select_db("projeto");
- 
-$login = mysql_query("select usuario, senha from pessoas where usuario = {$usuario} and senha = {$senha}");
-
-if($login) {
-    print "Bem vindo!!";
-}else {
-    print "Usuário e/ou senha inválidos!";
-}
-
-?>
-
 <?php
 
  include_once('../config/conexao_remoto.php');
