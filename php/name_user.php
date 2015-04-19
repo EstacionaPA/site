cc4c8b58b3887329fe317a351ad1508aad03d8dd
Andre Luiz <brasilandre93@hotmail.com>
2015-04-19 15:33:40 -0300

    Adicionado código para mostrar o nome do usuário no menu bar após o login

---
+++ php/name_user.php
@@ -0,0 +1,24 @@
+<?php
+
+session_start();
+
+include_once('modo_acesso.php');
+
+//Busca usuário na seção
+$usuario = $_SESSION['usuario'];
+
+//Realiza consulta
+$sql = mysql_query("SELECT nome FROM pessoas 
+        WHERE usuario ='$usuario'");
+
+//Retira o resultado da consulta na primeira linha "0"
+$result = mysql_result($sql, 0);
+
+//Retira a posição do primeiro espaço
+$posString = strpos($result, ' ');
+//Retira o primeiro nome (com a posição do primeiro espaço)
+$newString = substr($result, 0, $posString);
+
+print $newString;
+		
+?>
