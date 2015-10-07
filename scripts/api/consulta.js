30879db295c4f51e43395b290ec3527a950236d5
alfredudu <alfredudu_007@hotmail.com>
2015-10-06 18:04:30 -0700

    Servidor

---
+++ scripts/api/consulta.js
@@ -0,0 +1,46 @@
+        $(document).ready(function() {
+
+            function consultarcep() {
+                
+                $("#cidade").val("");
+                $("#estado").val("");
+            }
+            
+            $("#cep").blur(function() {
+
+                var cep = $(this).val().replace(/\D/g, '');
+
+                if (cep != "") {
+
+                    var validarcep = /^[0-9]{8}$/;
+
+                    if(validarcep.test(cep)) {
+
+                        $("#cidade").val("...")
+                        $("#estado").val("...")
+                       
+
+                        //Consulta ao webservice
+                        $.getJSON("//viacep.com.br/ws/"+ cep +"/json/?callback=?", function(dados) {
+
+                            if (!("erro" in dados)) {
+                                $("#cidade").val(dados.localidade);
+                                $("#estado").val(dados.uf);
+                            } //end if.
+                            else {
+                                consultarcep();
+                                alert("CEP não localizado, por favor verifique.");
+                            }
+                        });
+                    } //end if.
+                    else {
+                        consultarcep();
+                        alert("Formato de CEP inválido.");
+                    }
+                } //end if.
+                else {
+
+                    consultarcep();
+                }
+            });
+        });
