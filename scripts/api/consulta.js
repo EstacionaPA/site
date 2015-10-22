7b75f34efd4aeba243fc72d2d2c16b2e1d757014
alfredudu <alfredudu_007@hotmail.com>
2015-10-22 14:09:07 -0700

    Servidor Alterações

--- scripts/api/consulta.js
+++ scripts/api/consulta.js
@@ -1,94 +1,55 @@
-<<<<<<< HEAD
-        $(document).ready(function() {
+$(document).ready(function() {
 
-            function consultarcep() {
-                
-                $("#cidade").val("");
-                $("#estado").val("");
-            }
-            
-            $("#cep").blur(function() {
 
-                var cep = $(this).val().replace(/\D/g, '');
+    function consultarcep() {
 
-                if (cep != "") {
+        $("#cidade").val("");
+        $("#estado").val("");
+    }
 
-                    var validarcep = /^[0-9]{8}$/;
+    $("#cep").blur(function() {
 
-                    if(validarcep.test(cep)) {
 
-                        $("#cidade").val("...")
-                        $("#estado").val("...")
-                       
+        var cep = $(this).val().replace(/\D/g, '');
 
-                        //Consulta ao webservice
-                        $.getJSON("//viacep.com.br/ws/"+ cep +"/json/?callback=?", function(dados) {
 
-                            if (!("erro" in dados)) {
-                                $("#cidade").val(dados.localidade);
-                                $("#estado").val(dados.uf);
-                            } //end if.
-                            else {
-                                consultarcep();
-                                alert("CEP não localizado, por favor verifique.");
-                            }
-                        });
+        if (cep != "") {
+
+
+            var validarcep = /^[0-9]{8}$/;
+
+
+            //Valida o formato do CEP.
+            if(validarcep.test(cep)) {
+
+
+                $("#cidade").val("...")
+                $("#estado").val("...")
+
+
+
+                //Consulta ao webservice
+                $.getJSON("//viacep.com.br/ws/"+ cep +"/json/?callback=?", function(dados) {
+
+
+                    if (!("erro" in dados)) {
+                        $("#cidade").val(dados.localidade);
+                        $("#estado").val(dados.uf);
                     } //end if.
                     else {
                         consultarcep();
-                        alert("Formato de CEP inválido.");
+                        alert("CEP não localizado, por favor verifique.");
                     }
-                } //end if.
-                else {
-
-                    consultarcep();
-                }
-            });
-=======
-        $(document).ready(function() {
-
-            function consultarcep() {
-                
-                $("#cidade").val("");
-                $("#estado").val("");
+                });
+            } //end if.
+            else {
+                consultarcep();
+                alert("Formato de CEP inválido.");
             }
-            
-            $("#cep").blur(function() {
-
-                var cep = $(this).val().replace(/\D/g, '');
-
-                if (cep != "") {
-
-                    var validarcep = /^[0-9]{8}$/;
-
-                    if(validarcep.test(cep)) {
-
-                        $("#cidade").val("...")
-                        $("#estado").val("...")
-                       
-
-                        //Consulta ao webservice
-                        $.getJSON("//viacep.com.br/ws/"+ cep +"/json/?callback=?", function(dados) {
-
-                            if (!("erro" in dados)) {
-                                $("#cidade").val(dados.localidade);
-                                $("#estado").val(dados.uf);
-                            } //end if.
-                            else {
-                                consultarcep();
-                                alert("CEP não localizado, por favor verifique.");
-                            }
-                        });
-                    } //end if.
-                    else {
-                        consultarcep();
-                        alert("Formato de CEP inválido.");
-                    }
-                } //end if.
-                else {
-
-                    consultarcep();
-                }
-            });
->>>>>>> 54cf57a1f94b7504385ac6f985b7c8520081ace5
-        });
+        } //end if.
+        else {
+            //cep sem valor, limpa formulário.
+            consultarcep();
+        }
+    });
+});
