63ae996a06b954675db8a22a0aa609890d3df9d4
alfredudu <alfredudu_007@hotmail.com>
2015-11-15 10:41:46 -0200

    Adicionado api de cadastro automático para as demais páginas e definido acesso correto do cliente

--- php/SQL/connection.php
+++ php/SQL/connection.php
@@ -3,15 +3,10 @@
 class Connection{
     
     public function Conn ($network){
-    
-        if ($network == "remoto") {
-            
-            $conexao = mysql_pconnect("192.186.234.65","alfredudu","Sn@keDoctor_007") or die ("Erro na conexão!");
-            $banco = mysql_select_db("projeto");
-        }
-        elseif($network == "local"){
+
+        if($network == "local"){
             $conexao = mysql_pconnect("localhost","root","") or die ("Erro na conexão!");
-            $banco = mysql_select_db("projeto");
+            $banco = mysql_select_db("estacionapa");
         }
         
         elseif($network == "localServer"){
