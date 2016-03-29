3ca5e774a94b066a7d9f0911a5613c10fbf07479
root <brasilandre93@hotmail.com>
2016-03-29 02:34:36 +0000

    Retirado o TRY/CATCH e incluido verificação por IFs

--- php/SQL/connection.php
+++ php/SQL/connection.php
@@ -3,27 +3,29 @@
 class Connection{
     
     public function Conn (){
-        try{
+        
+        
+        $servidor = 'localhost';
+        $usuario = 'u650072308_site';
+        $senha = 'x3Lae6vUSI';
+        $banco = 'u650072308_site';
+        
+        $mysqli = @new mysqli($servidor, $usuario, $senha, $banco);
+        
+        if($mysqli->connect_error){
+       
             $servidor = 'localhost';
             $usuario = 'root';
             $senha = '';
             $banco = 'projeto';
 
             $mysqli = @new mysqli($servidor, $usuario, $senha, $banco);
-        }
-        catch(Exception $e){
-            $servidor = 'localhost';
-            $usuario = 'u650072308_site';
-            $senha = 'x3Lae6vUSI';
-            $banco = 'u650072308_site';
-
-            $mysqli = @new mysqli($servidor, $usuario, $senha, $banco);
             
-            if ($mysqli->connect_error) {
+            if($mysqli->connect_error){
                 die("A conexao falhou: " . $mysqli->connect_error);
-                return NULL;
             }
         }
+        
         
         return $mysqli;
 
